cdfb5d61bb4c7dd859d15b4b6f7a98222fcd8f5d
Stephane Moyart <moyartstephane@gmail.com>
2025-01-28 02:13:08 +0100

    feat (user layout) setting up the navbar + some UI

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,4 +1,4 @@
-import { LogOut } from "lucide-react"
+import { House, LinkIcon, LogOut, MessageSquare, Ticket } from "lucide-react"
 import { redirect } from "next/navigation"
 import { deleteSession } from "@/auth/session"
 import { SessionAvatar } from "@/auth/session-avatar"
@@ -8,16 +8,36 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <div className="flex w-full h-screen">
             <div className="flex flex-col items-center justify-between h-screen min-w-50 border-r border-black/20 shadow shadow-black/20">
-                <Link className="h-[50px] w-[50px] pb-2 opacity-100 hover:opacity-90" href="/account">
-                    <SessionAvatar height={50} width={50}/>
-                </Link>
+                <div className="flex flex-col w-full">
+                    <Link className="flex self-center h-[50px] w-[50px] my-4 rounded-full overflow-hidden opacity-100 hover:opacity-90" href="/account">
+                        <SessionAvatar height={50} width={50}/>
+                    </Link>
+
+                    <Link className="flex w-full items-center pl-4 gap-4 h-12 hover:bg-black/20" href="/dashboard">
+                        <House />
+                        <p className="max-md:hidden">Dashboard</p>
+                    </Link>
+                    <Link className="flex w-full items-center pl-4 gap-4 h-12 hover:bg-black/20" href="/conversations">
+                        <MessageSquare />
+                        <p className="max-md:hidden">Conversations</p>
+                    </Link>
+                    <Link className="flex w-full items-center pl-4 gap-4 h-12 hover:bg-black/20" href="/events">
+                        <Ticket />
+                        <p className="max-md:hidden">Evenements</p>
+                    </Link>
+                    <Link className="flex w-full items-center pl-4 gap-4 h-12 hover:bg-black/20" href="/links">
+                        <LinkIcon />
+                        <p className="max-md:hidden">Links</p>
+                    </Link>
+                </div>
+
                 <button
                     onClick={async () => {
                         "use server"
                         await deleteSession()
                         redirect('/signin')
                     }}
-                    className="flex items-center h-10 p-2 rounded-sm hover:bg-white" type="submit">
+                    className="flex items-center w-full h-12 justify-center gap-4 cursor-pointer hover:bg-orange-300" type="submit">
                     <LogOut />
                     <p className="max-md:hidden">Deconnexion</p>
                 </button>
