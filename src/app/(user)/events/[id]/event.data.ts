7b3fee2d123aae01a916c20c0abe1c738d22574e
Stephane Moyart <moyartstephane@gmail.com>
2025-03-05 01:02:13 +0100

    typescript (event) typing participativeListWithItems query

--- src/app/(user)/events/[id]/event.data.ts
+++ src/app/(user)/events/[id]/event.data.ts
@@ -1,5 +1,18 @@
 import { getSession } from "@/auth/session"
+import { UserBase } from "@/data/get-events"
 import { query } from "@/db/db"
+
+type ListItem = {
+    title: string
+    handled_by: UserBase
+}
+
+type ListT = {
+    id: string
+    event_id: string
+    title: string
+    items: ListItem[]
+}
 
 export const getParticipativeListsWithItems = async (eventId: string) => {
     //shield
@@ -7,7 +20,7 @@ export const getParticipativeListsWithItems = async (eventId: string) => {
     // end shield
 
     // todo check if sessionId is memeber of eventId
-    const lists = await query('SELECT el.*, COALESCE(jsonb_agg(jsonb_build_object(\'title\', eli.title, \'handled_by\', jsonb_build_object(\'id\', p.id, \'firstname\', p.firstname, \'lastname\', p.lastname, \'avatar\', p.avatar))), \'[]\') AS items FROM event_list el LEFT JOIN event_list_item eli ON el.id = eli.event_list_id LEFT JOIN person p ON eli.handled_by = p.id WHERE el.event_id = $1 GROUP BY el.id', [eventId])
+    const lists = await query<ListT>('SELECT el.*, COALESCE(jsonb_agg(jsonb_build_object(\'title\', eli.title, \'handled_by\', jsonb_build_object(\'id\', p.id, \'firstname\', p.firstname, \'lastname\', p.lastname, \'avatar\', p.avatar))), \'[]\') AS items FROM event_list el LEFT JOIN event_list_item eli ON el.id = eli.event_list_id LEFT JOIN person p ON eli.handled_by = p.id WHERE el.event_id = $1 GROUP BY el.id', [eventId])
     console.log(lists);
     return lists
 
